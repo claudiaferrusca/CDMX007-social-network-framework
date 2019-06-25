ebeb013ce9f7477c70ee3fc1fe5f5d94f93aec34
analhi7 <analhi.e.m7@gmail.com>
2019-06-25 13:28:05 -0500

    Actualiza metodo para visualizar imagen de usuario/ error 403 forbidden

--- Betwart/src/app/components/services/user.service.ts
+++ Betwart/src/app/components/services/user.service.ts
@@ -79,7 +79,7 @@ export class UserService {
 
     }
 
-    updateUser (user : User):Observable<any>{
+    updateUser(user:User):Observable<any>{
         let params =  JSON.stringify(user);
         let headers = new HttpHeaders().set('Content-Type', 'application/json')
                                        .set('Authorization', this.getToken());
